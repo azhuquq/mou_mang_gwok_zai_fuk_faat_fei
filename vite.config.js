a4968d55f559141613075185fa41abdb62beb0fe
azhuquq <i@azhuquq.com>
2025-02-09 23:11:56 +0800

    Add PWA support (test needed)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
@@ -10,6 +10,43 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Ticket Getter',
+        short_name: 'Ticket',
+        description: 'A 6X Boost Ticket Getter Web Application',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: "/web-app-manifest-192x192.png",
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
     // vueDevTools(),
   ],
   json: {
